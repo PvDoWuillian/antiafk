d27673f618d14ebd2c1c2a33afeaa650d286e407
Wuillian <r83238821@gmail.com>
2026-08-08 20:28:36 -0300

    Initialize Minecraft bot and web server
    Set up a Minecraft bot with a web server for monitoring.

---
+++ bot.js
@@ -0,0 +1,35 @@
+const mineflayer = require('mineflayer')
+const http = require('http')
+
+// Servidor web exigido pelo plano Free do Render
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Bot Online\n')
+})
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Servidor de monitoramento rodando na porta ${PORT}`)
+})
+
+// Configuração do robô de Minecraft
+const bot = mineflayer.createBot({
+  host: 'bmxsmp.playserver.pro', 
+  port: 25194,        
+  username: 'AntiAFK_Bot',
+  version: '1.21.1' 
+})
+
+bot.on('spawn', () => {
+  console.log('Bot entrou no servidor com sucesso!')
+  setInterval(() => {
+    bot.chat('Mantendo o servidor ativo...');
+  }, 180000)
+})
+
+bot.on('kick', (reason) => {
+  console.log('O bot foi expulso por: ' + reason)
+})
+
+bot.on('error', (err) => {
+  console.log('Erro encontrado: ', err)
+})
